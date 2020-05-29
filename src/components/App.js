04ec662a411e910f989472abfb48a9538d00e41f
Matto <matt.mcquain@gmail.com>
2020-05-28 23:06:21 -0700

    importing components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './Header';
 import Player from './Player';
 
 
-class App extends React.Component {
+class App extends Component {
   state = {
     players: [
       {
